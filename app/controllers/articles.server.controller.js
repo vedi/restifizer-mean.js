2cf03727591438b3bb4a7b81d72a9f1cb8adf6cc
vedi <shubin.fedor@gmail.com>
2014-10-28 14:31:37 +0300

    clean up articles

--- app/controllers/articles.server.controller.js
+++ app/controllers/articles.server.controller.js
@@ -1,109 +1,43 @@
+/**
+ * Created by vedi on 11/15/13.
+ */
 'use strict';
+var
+  _ = require('lodash'),
+  Q = require('q'),
+  HTTP_STATUSES = require('http-statuses'),
+  Article = require('mongoose').model('Article'),
+  BaseController = require('./base.server.controller')
+;
 
-/**
- * Module dependencies.
- */
-var mongoose = require('mongoose'),
-	errorHandler = require('./errors'),
-	Article = mongoose.model('Article'),
-	_ = require('lodash');
+var ArticleController = BaseController.extend({
+  ModelClass: Article,
+  path: '/api/articles',
+  fields: ['title', 'content', 'user', 'created'],
+  qFields: ['title'],
+  defaultOptions: _.defaults({
+    queryPipe: function (query, req, res, callback) {
+      return query.populate('user', 'displayName', callback);
+    }
+  }, BaseController.prototype.defaultOptions),
+  updateOptions: {
+    pre: function(req, res, callback) {
+      if (req.restifizer.action !== 'delete' && req.param('user') !== req.user.id) {
+        return callback(HTTP_STATUSES.FORBIDDEN.createError('Cannot change alone articles'));
+      }
+      callback();
+    }
+  },
+  prepareData: function (req, res, callback) {
+    callback(null, {user: req.user});
+  },
+  beforeDelete: function (doc, req, callback) {
+    if (doc.user.toString() !== req.user.id) {
+      return callback(HTTP_STATUSES.FORBIDDEN.createError('Cannot change alone articles'));
+    }
+    callback();
+  }
+});
 
-/**
- * Create a article
- */
-exports.create = function(req, res) {
-	var article = new Article(req.body);
-	article.user = req.user;
+module.exports = ArticleController;
 
-	article.save(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(article);
-		}
-	});
-};
-
-/**
- * Show the current article
- */
-exports.read = function(req, res) {
-	res.jsonp(req.article);
-};
-
-/**
- * Update a article
- */
-exports.update = function(req, res) {
-	var article = req.article;
-
-	article = _.extend(article, req.body);
-
-	article.save(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(article);
-		}
-	});
-};
-
-/**
- * Delete an article
- */
-exports.delete = function(req, res) {
-	var article = req.article;
-
-	article.remove(function(err) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(article);
-		}
-	});
-};
-
-/**
- * List of Articles
- */
-exports.list = function(req, res) {
-	Article.find().sort('-created').populate('user', 'displayName').exec(function(err, articles) {
-		if (err) {
-			return res.status(400).send({
-				message: errorHandler.getErrorMessage(err)
-			});
-		} else {
-			res.jsonp(articles);
-		}
-	});
-};
-
-/**
- * Article middleware
- */
-exports.articleByID = function(req, res, next, id) {
-	Article.findById(id).populate('user', 'displayName').exec(function(err, article) {
-		if (err) return next(err);
-		if (!article) return next(new Error('Failed to load article ' + id));
-		req.article = article;
-		next();
-	});
-};
-
-/**
- * Article authorization middleware
- */
-exports.hasAuthorization = function(req, res, next) {
-	if (req.article.user.id !== req.user.id) {
-		return res.status(403).send({
-			message: 'User is not authorized'
-		});
-	}
-	next();
-};
