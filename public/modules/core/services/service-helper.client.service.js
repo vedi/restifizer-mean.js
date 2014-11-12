8f48ee8ad8c35e0309a9da11a30d26949897860d
vedi <shubin.fedor@gmail.com>
2014-11-12 23:59:40 +0300

    implement ServiceHelper

---
+++ public/modules/core/services/service-helper.client.service.js
@@ -0,0 +1,20 @@
+'use strict';
+
+angular.module('core').factory('ServiceHelper', ['Restangular',
+  function(Restangular) {
+    this.createRestfulResource = function createRestfulResource(route, fields, defaults) {
+      var restResource = Restangular.all(route);
+      restResource.fields = fields;
+      restResource.defaults = defaults;
+      restResource.restangularizeElement = function(parent, element, queryParams) {
+        Restangular.restangularizeElement(parent, element, route, queryParams);
+      };
+      restResource.restangularizeCollection = function(parent, element, queryParams) {
+        Restangular.restangularizeCollection(parent, element, route, queryParams);
+      };
+      return restResource;
+    };
+
+    return this;
+  }
+]);
